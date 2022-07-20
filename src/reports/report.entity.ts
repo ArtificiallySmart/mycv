95b0bae6bab8bc20773688b6596159b9eca03938
ArtificiallySmart <bryanparcival@gmail.com>
2022-07-20 17:36:00 +0200

    replaced currentUser interceptor with middleware

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,10 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { User } from 'src/users/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class Report {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    price: number;
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  mileage: number;
+
+  @Column({ default: false })
+  approved: boolean;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,29 +1,43 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
-
+import { Report } from 'src/reports/report.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+  OneToMany,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @AfterInsert()
-    logInsert(){
-        console.log('inserted user with id', this.id);
-    }
+  @Column({ default: true })
+  admin: boolean;
 
-    @AfterUpdate()
-    logUpdate(){
-        console.log('updated user with id', this.id);
-    }
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
-    @AfterRemove()
-    logRemove(){
-        console.log('removed user with id', this.id);
-    }
+  @AfterInsert()
+  logInsert() {
+    console.log('inserted user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id', this.id);
+  }
 }

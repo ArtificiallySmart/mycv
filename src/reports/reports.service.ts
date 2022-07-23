06e0bfdb6b895fa072f75c4e017acaf0978bca86
ArtificiallySmart <bryanparcival@gmail.com>
2022-07-23 10:25:58 +0200

    Added price generation logic

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,10 +4,34 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createEstimate({
+    make,
+    model,
+    longitude,
+    latitude,
+    year,
+    mileage,
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('longitude - :longitude BETWEEN -5 AND 5', { longitude })
+      .andWhere('latitude - :latitude BETWEEN -5 AND 5', { latitude })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
